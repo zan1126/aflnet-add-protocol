2506e44161b432bd6c8608797f8ba1dfdbaedacc
zan1126 <59103104+zan1126@users.noreply.github.com>
2020-10-02 10:29:06 +0800

    Update afl-fuzz.c

--- afl-fuzz.c
+++ afl-fuzz.c
@@ -1,8 +1,8 @@
 //add NGTCP in the main function
 case 'P': 
 	     if (!strcmp(optarg, "NGTCP")) {
-          extract_requests = &extract_requests_quic;
-          extract_response_codes = &extract_response_codes_quic;
+          extract_requests = &extract_requests_ngtcp;
+          extract_response_codes = &extract_response_codes_ngtcp;
 	     } else {
           FATAL("%s protocol is not supported yet!", optarg);
        }
